3a3fd24ab79bdd0fa90ba3cfc715c4dfcfa57239
graceniyigena34 <graceniyigena34@gmail.com>
2026-01-09 12:21:13 +0200

    Created the cart file in the route which shows the authentication of get, post, delete and put and also give the cart datatype in the models

---
+++ src/routes/cart.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import {  } from '../controllers/cartController.js';
+
+export const cartRouter = Router();
+
+cartRouter.get('/:userId', );
+cartRouter.post('/:userId/items', );
+cartRouter.put('/:userId/items/:id', );
+cartRouter.delete('/:userId/items/:id', );
+cartRouter.delete('/:userId', );
